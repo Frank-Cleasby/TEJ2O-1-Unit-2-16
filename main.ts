49038fcdebec97b39d2e5f1f3d518b386bfbdd72
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-11-06 13:29:56 -0500

    works

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Frank
+ * Created on: Nov 2025
+ * This program using a radio feature
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceOfObstacle: number = 0
+
+// cleanse
+radio.setGroup(39)
+basic.clearScreen()
+basic.showIcon(IconNames.Giraffe)
+
+// grabs distance
+while (true) {
+   distanceOfObstacle = sonar.ping(
+    DigitalPin.P12, 
+    DigitalPin.P13, 
+    PingUnit.Centimeters
+    )
+    if (distanceOfObstacle < 10) {
+        radio.sendString('Too Close bub')
+    }
+    radio.onReceivedString(function(receivedString: string) {
+        basic.clearScreen()
+        basic.showString(receivedString)
+        basic.showIcon(IconNames.Happy)
+    })
+}
